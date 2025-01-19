9f4da63aed1ff91ff518862d8900c17b62521ddf
John Walsh <jwalsh2@gmail.com>
2025-01-19 08:54:20 -0600

    added in UV virtual env and bumped to Py 3.13

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gonet-image-utils
+
+Updated to add in UV virtual environment with Python 3.13
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from gonet-image-utils!")
+
+
+if __name__ == "__main__":
+    main()
